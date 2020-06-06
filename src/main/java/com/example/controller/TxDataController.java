baf74a2cc6a444494c64fef3220596bdab85cb16
Vikrant Gupta <vikrant.gupta@aarnet.edu.au>
2020-06-06 23:48:44 +1000

    working code for file download

--- src/main/java/com/example/controller/TxDataController.java
+++ src/main/java/com/example/controller/TxDataController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +13,10 @@ import com.example.model.ClientInformation;
 import com.example.model.ProductInformation;
 import com.example.service.FixedWidthDataParser;
 import com.example.service.TxDataService;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +24,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class TxDataController {
@@ -28,9 +35,9 @@ public class TxDataController {
 
 	@Autowired
 	FixedWidthDataParser fileService;
-	
+
 	@GetMapping("/")
-	public Map<ClientInformation,Map<ProductInformation, List<TxData>>> getTxDataList() {
+	public void getTxDataList(HttpServletResponse response) {
 		List<TxData> dataList = txDataService.findAll();
 		Map<ClientInformation,Map<ProductInformation, List<TxData>>> result = new HashMap<>();
 		Map<ClientInformation, List<TxData>> clientTxData = dataList.stream()
@@ -48,20 +55,41 @@ public class TxDataController {
 
 		}
 
-		for (ClientInformation clientInformation: result.keySet()) {
 
-			Map<ProductInformation, List<TxData>> clientProductTxData = result.get(clientInformation);
-			for (ProductInformation productInformation: clientProductTxData.keySet()) {
-				List<TxData> txData = clientProductTxData.get(productInformation);
-				int Total_Transaction_Amount =0;
-				for (TxData txData1: txData){
-					Total_Transaction_Amount+=(txData1.getQuantityLong()-txData1.getQuantityShort());
+
+		//set file name and content type
+		String filename = "users.csv";
+
+		response.setContentType("text/csv");
+		response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + filename + "\"");
+
+		try {
+			response.setContentType("text/csv");
+			response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+					"attachment; filename=\"" + filename + "\"");
+			CSVPrinter csvPrinter = new CSVPrinter(response.getWriter(),
+					CSVFormat.DEFAULT.withHeader("field1", "field2", "field3"));
+
+			for (ClientInformation clientInformation: result.keySet()) {
+
+				Map<ProductInformation, List<TxData>> clientProductTxData = result.get(clientInformation);
+				for (ProductInformation productInformation: clientProductTxData.keySet()) {
+					List<TxData> txData = clientProductTxData.get(productInformation);
+					int Total_Transaction_Amount =0;
+					for (TxData txData1: txData){
+						Total_Transaction_Amount+=(txData1.getQuantityLong()-txData1.getQuantityShort());
+					}
+					csvPrinter.printRecord(Arrays.asList(clientInformation, productInformation,Total_Transaction_Amount));
+					System.out.println(clientInformation+""+productInformation+""+Total_Transaction_Amount);
 				}
-				System.out.println(clientInformation+""+productInformation+""+Total_Transaction_Amount);
-			}
 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return result;
+
+
 	}
 
 	@PostMapping("/upload")
